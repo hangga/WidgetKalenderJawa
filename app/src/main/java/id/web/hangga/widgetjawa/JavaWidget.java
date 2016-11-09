a5e5786a4611a6041cc186e83dde59a61021a969
sayekti <sayekti@sebangsa.com>
2016-11-09 17:56:35 +0700

    paling sip

--- app/src/main/java/id/web/hangga/widgetjawa/JavaWidget.java
+++ app/src/main/java/id/web/hangga/widgetjawa/JavaWidget.java
@@ -4,10 +4,9 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-
-import java.util.Calendar;
 
 
 /**
@@ -15,50 +14,41 @@ import java.util.Calendar;
  */
 public class JavaWidget extends AppWidgetProvider {
 
-    private PendingIntent service = null;
-    public static String ACTION_WIDGET_RECEIVER = "ActionReceiverWidget";
-
-    /*static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
-
-        Calendar calendar = Calendar.getInstance();
-
-        CharSequence widgetText = context.getString(R.string.app_name);
-        // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.java_widget);
-        views.setTextViewText(R.id.txt_hari, JavaneseCalenderUtils.getDina(calendar));
-        views.setTextViewText(R.id.txt_pasaran, JavaneseCalenderUtils.getPasaran(calendar));
-        views.setTextViewText(R.id.txt_date_hijri, HijriCalendar.getSimpleDate(calendar));
-        views.setTextViewText(R.id.txt_date_georgian, new SimpleDateFormat("dd-MMMM-yyyy HH:mm:ss").format(calendar.getTime()));
-
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
-
-
-    }*/
+    //private PendingIntent service = null;
+    //public static String ACTION_WIDGET_RECEIVER = "ActionReceiverWidget";
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        // There may be multiple widgets active, so update all of them
-        /*for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
-        }*/
-        Calendar TIME = Calendar.getInstance();
+        ComponentName componentName = new ComponentName(context, JavaWidget.class);
+        int[] allWidgetIds = appWidgetManager.getAppWidgetIds(componentName);
+
+        Intent intent = new Intent(context.getApplicationContext(), MyService.class);
+        intent.setAction(MyService.ACTION_UPDATE);
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, allWidgetIds);
+        context.startService(intent);
+
+        PendingIntent pendingIntent = PendingIntent.getService(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
         AlarmManager m = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-        Intent i = new Intent(context, MyService.class);
-        if (service == null){
-            service = PendingIntent.getService(context, 0, i, PendingIntent.FLAG_CANCEL_CURRENT);
-        }
-        m.setRepeating(AlarmManager.RTC, TIME.getTime().getTime(), 1000 * 1/*lima detik*/, service);
+        m.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), 1000 * 1, pendingIntent);
     }
 
     @Override
     public void onEnabled(Context context) {
-        // Enter relevant functionality for when the first widget is created
+        super.onEnabled(context);
     }
 
     @Override
     public void onDisabled(Context context) {
-        // Enter relevant functionality for when the last widget is disabled
+        super.onDisabled(context);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+        ComponentName componentName = new ComponentName(context, JavaWidget.class);
+        onUpdate(context, appWidgetManager, appWidgetManager.getAppWidgetIds(componentName));
     }
 }
 
--- app/src/main/java/id/web/hangga/widgetjawa/MyService.java
+++ app/src/main/java/id/web/hangga/widgetjawa/MyService.java
@@ -2,9 +2,9 @@ package id.web.hangga.widgetjawa;
 
 import android.app.Service;
 import android.appwidget.AppWidgetManager;
-import android.content.ComponentName;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 import android.widget.RemoteViews;
 
 import java.text.SimpleDateFormat;
@@ -15,22 +15,39 @@ import id.web.hangga.widgetjawa.Utils.JavaneseCalenderUtils;
 
 public class MyService extends Service {
 
-    Calendar calendar = Calendar.getInstance();
+    Calendar calendar;
+    public static String ACTION_UPDATE = "update";
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        update();
+        if (intent == null) {
+            return super.onStartCommand(null, flags, startId);
+        }
+
+        String action = intent.getAction();
+        if (action == null) {
+            return super.onStartCommand(intent, flags, startId);
+        }
+
+        Log.d("JAWAW", String.format("onStartCommand: %s", action));
+
+        if (action.equals(ACTION_UPDATE)) {
+            int[] allWidgetIds = intent.getIntArrayExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS);
+            for (int widgetId : allWidgetIds) {
+                update(widgetId);
+            }
+        }
         return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return null;
     }
 
-    private void update(){
-
+    private void update(int widgetId){
+        Log.d("JAWAW", "yak update view");
+        calendar = Calendar.getInstance();
         RemoteViews views = new RemoteViews(getPackageName(), R.layout.java_widget);
         views.setTextViewText(R.id.txt_hari, JavaneseCalenderUtils.getDina(calendar));
         views.setTextViewText(R.id.txt_pasaran, JavaneseCalenderUtils.getPasaran(calendar));
@@ -38,8 +55,7 @@ public class MyService extends Service {
         views.setTextViewText(R.id.txt_date_georgian, new SimpleDateFormat("dd-MMMM-yyyy HH:mm:ss").format(calendar.getTime()));
 
         // Push update for this widget to the home screen
-        ComponentName thisWidget = new ComponentName(this, JavaWidget.class);
         AppWidgetManager manager = AppWidgetManager.getInstance(this);
-        manager.updateAppWidget(thisWidget, views);
+        manager.updateAppWidget(widgetId, views);
     }
 }
